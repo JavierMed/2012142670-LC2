8c1bddc238834a3369e930cbaa19af138e0a145a
JavierMed <josejavier.medina.huaraca@gmail.com>
2017-05-14 04:30:27 -0500

    create attrib for the class

---
+++ 2012142670-SLN/2012142670-PER/EntityTypeConfiguration/AdministrarEquipoConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using _2012142670_ENT.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2012142670_PER.EntityTypeConfiguration
+{
+    public class AdministrarEquipoConfiguration:EntityTypeConfiguration<AdministradorEquipo>
+    {
+    }
+}
---
+++ 2012142670-SLN/2012142670-PER/EntityTypeConfiguration/DepartamentoConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using _2012142670_ENT.Entities;
+using System.Data.Entity.ModelConfiguration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2012142670_PER.EntityTypeConfiguration
+{
+    public class DepartamentoConfiguration : EntityTypeConfiguration<Departamento>
+    {
+    }
+}
